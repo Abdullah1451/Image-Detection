151a2fc899dbc9251b9a7b3dc22b0ff686ce3241
Abdullah <53345272+Abdullah1451@users.noreply.github.com>
2022-03-28 01:09:11 +0530

    comment addded

--- detection.py
+++ detection.py
@@ -5,7 +5,7 @@ execution_path = os.getcwd()
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+detector.setModelPath( os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))#using ML model 'resnet50_coco_best_v2.0.1.h5' to detect all object and persons
 detector.loadModel()
 
 detections = detector.detectObjectsFromImage(
